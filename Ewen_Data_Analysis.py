45685045a77ecb2ec434dbf403117eb03ea141a8
EwenBernard <73660111+EwenBernard@users.noreply.github.com>
2021-09-30 19:58:11 +0200

    Data Year Chart

--- Ewen_Data_Analysis.py
+++ Ewen_Data_Analysis.py
@@ -4,8 +4,8 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import plotly.graph_objects as go
-
+import plotly.express as px
+"""
 def plotly_bar_chart(
         df: pd.DataFrame,
         x_axis_label: str = 'Month',
@@ -14,17 +14,17 @@ def plotly_bar_chart(
     df.values
     df.columns
     this_chart = go.Figure(
-        data=[go.Bar(x=df.columns, y=df.values, text = df.values,textposition = 'auto')])
+        data=[go.Bar(x=df.columns, y=df.values,textposition = 'auto')])
     this_chart.update_yaxes(title_text=y_axis_label)
     this_chart.update_xaxes(title_text=x_axis_label)
 
     return this_chart
-
+"""
 st.title('Ewen Life')
 
 year = {"2015" : None, "2016" : None, "2017" : None, "2018" : None, "2019" : None, "2020" : None, "2021" : None}
 month = {"JANUARY" : [], "FEBRUARY" : [], "MARCH" : [], "APRIL" : [], "MAY" : [], "JUNE" : [], "JULY" : [], "AUGUST" : [], "SEPTEMBER" : [], "OCTOBER" : [], "NOVEMBER" : [], "DECEMBER" : []}
-path = "C:/Users/ebern/Desktop/Notebook/Extracted_Google_Maps_Data"
+path = "C:/Users/ebern/Desktop/GitHub/Data_Visualisation/Extracted_Google_Maps_Data"
 year_list = []
 for folder in os.listdir(path):
     month = {"JANUARY" : [], "FEBRUARY" : [], "MARCH" : [], "APRIL" : [], "MAY" : [], "JUNE" : [], "JULY" : [], "AUGUST" : [], "SEPTEMBER" : [], "OCTOBER" : [], "NOVEMBER" : [], "DECEMBER" : []}
@@ -44,12 +44,14 @@ for key_year, val_year in year.items():
     for key, value in val_year.items():
         val_sum = 0
         val_sum += len(value)
-        month_sum[key] = [val_sum]
-    month_sum = pd.DataFrame.from_dict(month_sum)
+        month_sum[key] = val_sum
     #month_sum.columns
     #month_sum.values
-    chart = plotly_bar_chart(df=month_sum)
-    st.plotly_chart(chart)
+    print(list(month_sum.keys()))
+    print(list(month_sum.values()))
+    dict = {key_year : list(month_sum.keys()), 'Number of data files': list(month_sum.values())}
+    fig = px.bar(dict, x=key_year, y= 'Number of data files')
+    st.plotly_chart(fig)
 #plt.legend(framealpha=1)
 
 #print(year_list[1])
